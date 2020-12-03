165a5063030b33e676c599d68e89529517159418
Ibrahim Sardar <isardar@funaiservice.com>
2020-12-03 18:25:43 -0500

    sometimes need to clear cache on init

--- drop-into-your-project/server/init.sh
+++ drop-into-your-project/server/init.sh
@@ -4,5 +4,7 @@ cd /var/www/html/laravel-project
 #php artisan migrate:fresh --seed
 chgrp -R www-data storage bootstrap/cache
 chmod -R ug+rwx storage bootstrap/cache
+php artisan cache:clear
+php artisan config:clear
 
 apachectl -D FOREGROUND
